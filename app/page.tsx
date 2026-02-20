9d2cc988057701dd64cc3ca13d5746dd1ecbabd0
Vihanga Jayasinghe <jayasinghevihanga2@gmail.com>
2026-02-20 08:19:09 +0530

    intro

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import UpcomingEvents from "@/components/UpcomingEvents";
 import Gallery from "@/components/Gallery";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import IntroModal from "@/components/IntroModal";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black">
+      <IntroModal />
       <Navbar />
       <Hero />
       <UpcomingEvents />
